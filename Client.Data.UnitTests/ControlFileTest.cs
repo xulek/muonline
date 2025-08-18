14af3c79e95f7c9b911d05749467e7138fc366d1
Your Name <your.email@example.com>
2025-08-18 10:27:18 +0700

    update control file test

---
+++ Client.Data.UnitTests/ControlFileTest.cs
@@ -0,0 +1,49 @@
+using Client.Data.CAP;
+using Client.Data.CWS;
+using Client.Data.OBJS;
+
+namespace Client.Data.UnitTests.Control
+{
+    [TestFixture]
+    public class Control_Should_Deserialize
+    {
+
+        private CAPReader capReader;
+        private CWSReader cwsReader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            capReader = new();
+            cwsReader = new();
+        }
+
+        [Test]
+        public async Task GenericCameraAnglePositionFileExists()
+        {
+            var path = Path.Combine(Constants.DataPath, "G_Camera_Angle_Position.bmd");
+            var genericCameraAnglePositionFile = File.Exists(
+                path
+            );
+            Assert.That(genericCameraAnglePositionFile, Is.True, $"Generic camera angle position file should exists");
+
+            CameraAnglePosition data = await capReader.Load(path);
+            Assert.That(data, Is.Not.Null, "CAP should be deserialized");
+        }
+        [Test]
+        public async Task CustomCameraAnglePositionFileExists()
+        {
+            var path = Path.Combine(Constants.DataPath, "World111", "Camera_Angle_Position.bmd");
+            var customCameraAnglePositionFile = File.Exists(
+                path
+            );
+            Assert.That(customCameraAnglePositionFile, Is.True, $"World111 camera angle position file should exists");
+            CameraAnglePosition data = await capReader.Load(path);
+
+            Assert.That(data, Is.Not.Null, "CAP should be deserialized");
+
+        }
+
+
+    }
+}
